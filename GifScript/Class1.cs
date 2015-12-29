0ecb693587e0244ffe2d10d479b8aedb21f7191d
LaurieCheers <lauriecheers@gmail.com>
2015-12-29 13:12:10 +0000

    Added giflib

--- GifScript/Class1.cs
+++ GifScript/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jillzhang.GifUtility;
 
 namespace GifScript
 {
     public class Class1
     {
+
     }
 }
---
+++ GifScriptTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using Jillzhang.GifUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GifScriptTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GifImage gifImage = GifDecoder.Decode(@"C:\Users\Laurie\Pictures\test.gif");
+
+        }
+    }
+}

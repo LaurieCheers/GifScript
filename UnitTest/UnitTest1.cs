f5f171d0592a7c27b37f8dc91fafd2176fb5a47e
Michael Cheers <michaelcheers8@gmail.com>
2015-12-29 14:18:10 +0100

    Added Unit Tests.

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jillzhang.GifUtility;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GifScriptTest()
+        {
+            GifImage gifImage = GifDecoder.Decode(@"C:\Users\Laurie\Pictures\test.gif");
+        }
+    }
+}

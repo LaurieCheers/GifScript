6ac1c0e285d798e04e8fa7aaa3e873037ed22906
LaurieCheers <lauriecheers@gmail.com>
2017-03-19 15:41:36 +0100

    Debugger should default to colorcube

--- GifScriptDebugger/Game1.cs
+++ GifScriptDebugger/Game1.cs
@@ -471,7 +471,7 @@ namespace GifScriptDebugger
         void DoRestart()
         {
             NGif.GifDecoder decoder = new NGif.GifDecoder();
-            if (decoder.Read("../../../../../Examples/worldmap2.gif") == 0)
+            if (decoder.Read("../../../../../Examples/colorcube2.gif") == 0)
             {
                 gifScriptState = new GifScriptState();
                 gifScriptState.Init(new GifCube(decoder));
